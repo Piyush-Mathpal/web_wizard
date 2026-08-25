aaf463344770e851719c5be78b326f5d2c6e3a77
Piyush Mathpal <piyushmathpal89@gmail.com>
2026-08-25 14:43:24 +0530

    fix github pages path

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/web_wizards/',
+  base:'/web_wizard/',
   plugins: [
     react(),
     tailwindcss()
